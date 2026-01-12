65acddc848e0bbfe06f15a0b282255cc0583de68
Augusto Galves <augustogalves.eng@gmail.com>
2026-01-12 14:26:42 -0300

    Fixes and Redesigning the site to be built using components - Part 3

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,3 +1,6 @@
 const CONTACT_EMAIL    = "mailto:pedroAGAsilva@email.com";
 const CONTACT_WHATSAPP = "https://wa.me/5521984095043";
+const CONTACT_PHONE = "tel:+552198409-5043";
 const CONTACT_GITHUB   = "https://github.com/PedroAGASilva";
+const CONTACT_LINKEDIN   = "https://www.linkedin.com/in/augustogomessilva/";
+const CONTACT_INSTAGRAM   = "https://www.instagram.com/pedro_agas";
