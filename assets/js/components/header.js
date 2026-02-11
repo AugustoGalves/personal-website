a2c78ac3970921fd7e5113d106dd187f754a1f02
Augusto Galves <augustogalves.eng@gmail.com>
2026-02-11 02:02:54 -0300

    Fixed more broken CSS and HTML flex layouts.

--- assets/js/components/header.js
+++ assets/js/components/header.js
@@ -51,7 +51,7 @@ class THeader extends HTMLElement {
         
 <header>
 <div class="container flex">   
-    <div class="flex flex-row justify-spacebetween">
+    <div class="flex flex-row justify-spacebetween w-full">
         
         <!-- ─── NAV BAR - LEFT SIDE ──────────────────────────────────────────────────────────────── ✣ ─ -->
         <a href="${links.navbutton1}"><img class="logo-header" src="${links.headerlogo_darktheme}" alt="Pedro AGA Silva, engenheiro mecânico"></a>
